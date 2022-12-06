ed3095d42b6b7a2b8fdbfa2d265312f566be4c0e
@DanielNitschkeVW <danielnitschkevw@gmail.com>
2022-12-06 06:09:15 +0100

    Solution for 2022 day 6 - one star and two stars

---
+++ src/Solutions/Year2022/Day6/Second.java
@@ -0,0 +1,31 @@
+package Solutions.Year2022.Day6;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Second {
+
+    public static String solver(List<String> list) {
+        String word = list.get(0);
+        final int SCAN_RANGE = 14;
+        int index = 0;
+        for(; index < word.length() - SCAN_RANGE; index++) {
+            if (match(word, index, SCAN_RANGE)) {
+                break;
+            }
+        }
+        return String.valueOf(index + SCAN_RANGE);
+    }
+
+    public static boolean match(String word, int index, int len) {
+        Set<Character> chars = new HashSet<>();
+        int matchSize = len;
+        for(; len > 0; len--, index++) {
+            chars.add(word.charAt(index));
+        }
+        return chars.size() == matchSize;
+    }
+
+    private Second() {}
+}
